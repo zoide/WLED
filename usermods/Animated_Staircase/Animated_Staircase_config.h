f54cfbdf5b9d27e847bc33570bdc321a70539976
Udo Waechter <udo@maketank.net>
2021-04-09 17:33:45 +0200

    Staircase

--- usermods/Animated_Staircase/Animated_Staircase_config.h
+++ usermods/Animated_Staircase/Animated_Staircase_config.h
@@ -5,17 +5,17 @@
  */
 
 // Please change the pin numbering below to match your board.
-#define TOP_PIR_PIN 5
-#define BOTTOM_PIR_PIN 6
+//#define TOP_PIR_PIN 5
+//#define BOTTOM_PIR_PIN 6
 
 // Or uncumment and a pir and use an ultrasound HC-SR04 sensor,
 // see README.md for details
 #ifndef TOP_PIR_PIN
-#define TOP_TRIGGER_PIN D2
-#define TOP_ECHO_PIN D3
+#define TOP_TRIGGER_PIN 5
+#define TOP_ECHO_PIN 4
 #endif
 
 #ifndef BOTTOM_PIR_PIN
-#define BOTTOM_TRIGGER_PIN D4
-#define BOTTOM_ECHO_PIN D5
+#define BOTTOM_TRIGGER_PIN 32
+#define BOTTOM_ECHO_PIN 33
 #endif
